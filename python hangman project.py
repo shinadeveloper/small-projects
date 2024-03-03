c1b8437412530f40db9eb1d61bb8b76a436812ef
Dilraj Singh <145498719+shinadeveloper@users.noreply.github.com>
2024-03-03 15:28:07 +0530

    new files

--- python hangman project.py
+++ python hangman project.py
@@ -3,19 +3,16 @@ import random
 # Function to print hangman
 def hangman():
   list=["apple", "banana", "orange", "grapes", "watermelon", "pineapple", "mango", "kiwi"]
-
   word=random.choice(list)
   print("Hello your name please")
   name=input()
   print("Hello",name,"Welcome to Hangman Game")
-  
   print("Guess the word")
   number=len(word)
   i=0
   print('_'*number)
   guessed=[]
   count=0
-
   while i < (number+4) and count<=number:
     count=0
     print("enter the alphabet")
@@ -23,7 +20,6 @@ def hangman():
     if (alph in word) :
       print("yay some more")
       guessed.append(alph)
-      
     for i in range(number):
       if word[i] in guessed:
         print(word[i],end="")
